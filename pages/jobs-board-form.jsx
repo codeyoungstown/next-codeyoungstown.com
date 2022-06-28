376e616a0a2a531b255ec741e725f38afca35051
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-06-28 16:27:37 -0400

    put boilerplate to avoid build errors

--- pages/jobs-board-form.jsx
+++ pages/jobs-board-form.jsx
@@ -4,3 +4,20 @@
 // - location (remote?)
 // - salary range (optional)
 // - job listing and/or contact person
+
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
+
+// Card design for each company with consulting badge
