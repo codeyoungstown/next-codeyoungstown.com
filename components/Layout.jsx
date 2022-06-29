74afae3f0ef2a10747936774c3ce43694e7e7eee
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-06-29 17:32:35 -0400

    created Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,48 +3,60 @@ import { useRouter } from "next/router";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
-export function Layout({ title, description, children }) {
+export default function Layout({ children }) {
   const router = useRouter();
+  const title = "Code Youngstown";
+  const description =
+    "Connecting software engineers, developers, and coders in the Youngstown, OH area. Join the ongoing chat via Slack and attend our meetups.";
 
   return (
-    <div className="max-w-screen-lg m-auto p-4 pb-1 min-h-screen flex flex-col">
+    <html lang="en">
       <Head>
+        {/*Thanks to https://megatags.co/ for generating these*/}
         <title>{title}</title>
-        <meta name="description" content={description} />
-
-        <link rel="icon" href="/favicon.ico" />
 
         {/* <!-- Primary Meta Tags --> */}
-        <title>{title}</title>
         <meta name="title" content={title} />
         <meta name="description" content={description} />
+        <link rel="icon" href="/code-youngstown-white-background-final.png" />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta
           property="og:url"
-          content={"https://JoeDuncko.com" + router.asPath}
+          content={"https://codeyoungstown.com" + router.asPath}
         />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {/* <meta property="og:image" content="" /> */}
+        <meta
+          property="og:image"
+          content="/code-youngstown-white-background-final.png"
+        />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
-          content={"https://JoeDuncko.com" + router.asPath}
+          content={"https://codeyoungstown.com" + router.asPath}
         />
         <meta property="twitter:title" content={title} />
         <meta property="twitter:description" content={description} />
-        {/* <meta property="twitter:image" content="" /> */}
+        <meta
+          property="twitter:image"
+          content="/code-youngstown-white-background-final.png"
+        />
+
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Header />
+      <div className="max-w-screen-lg m-auto p-4 pb-1 min-h-screen flex flex-col bg-gray text-white text-center flex-1">
+        <Header />
 
-      <main className="flex-grow flex flex-col">{children}</main>
+        <main className="flex-grow flex flex-col">{children}</main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </html>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Script from "next/script";
+import Layout from "../components/Layout";
+{
+  /* 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookOfficial,
@@ -12,51 +12,39 @@ import {
   faPodcast,
   faEnvelope,
 } from "@fortawesome/free-solid-svg-icons";
-
+*/
+}
 export default function Home() {
   return (
-    <div className="bg-gray text-white text-center flex-1">
-      <Head>
-        <title>Code Youngstown</title>
-        <meta
-          name="description"
-          content="Connecting software engineers, developers, and coders in the Youngstown, OH area. Join the ongoing chat via Slack and attend our meetups."
-        />
-        <link
-          rel="icon"
-          href="/public/code-youngstown-white-background-final.png"
-        />
-      </Head>
+    <Layout>
+      <Image
+        src="/code-youngstown-white-background-final.png"
+        alt="Code Youngstown Logo"
+        height={400}
+        width={400}
+      />
 
-      <main>
-        <Header />
-        <Image
-          src="/code-youngstown-white-background-final.png"
-          alt="Code Youngstown Logo"
-          height={400}
-          width={400}
-        />
+      <h1 className="text-3xl">
+        Connecting software engineers, developers, and coders in the Youngstown,
+        OH area.
+      </h1>
+      <h2 className="text-2xl">
+        Join the ongoing chat via{" "}
+        <a className="underline" href="http://slack.codeyoungstown.com/">
+          Slack
+        </a>{" "}
+        and attend our{" "}
+        <a className="underline" href="https://www.meetup.com/Code-Youngstown/">
+          meetups
+        </a>
+        .
+      </h2>
 
-        <h1 className="text-3xl">
-          Connecting software engineers, developers, and coders in the
-          Youngstown, OH area.
-        </h1>
-        <h2 className="text-2xl">
-          Join the ongoing chat via{" "}
-          <a className="underline" href="http://slack.codeyoungstown.com/">
-            Slack
-          </a>{" "}
-          and attend our{" "}
-          <a
-            className="underline"
-            href="https://www.meetup.com/Code-Youngstown/"
-          >
-            meetups
-          </a>
-          .
-        </h2>
-        <Footer />
-      </main>
-    </div>
+      <Script
+        async
+        defer
+        src="https://slack.codeyoungstown.com/slackin.js?large"
+      />
+    </Layout>
   );
 }
