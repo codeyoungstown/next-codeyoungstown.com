7b0a672ff721d10dac2b9dedda81209721926980
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-07-04 14:06:30 -0400

    added basic layout to all other pages

---
+++ pages/jobs-form.tsx
@@ -0,0 +1,24 @@
+import Layout from "../components/Layout";
+
+export default function JobsForm() {
+  return (
+    <Layout>
+      <h1 className="text-3xl p-2">
+        Add a listing to be featured on the Jobs Board
+      </h1>
+
+      <form>
+        {/* 
+      // - referring person
+      // - job title
+      // - company name
+      // - location (remote?)
+      // - salary range (optional)
+      // - job listing and/or contact person 
+      */}
+      </form>
+    </Layout>
+  );
+}
+
+// Card design for each company with consulting badge
