c755480d99f51bf8fa0096c58c5437521d28b211
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-07-04 15:28:09 -0400

    mobile optimization stuff

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -3,7 +3,7 @@ import { socials } from "../data/socials";
 
 export default function Socials() {
   return (
-    <ul className="flex flex-row items-center text-center gap-8 justify-center text-4xl mb-4">
+    <ul className="flex flex-wrap flex-row items-center text-center gap-8 justify-center text-4xl mb-4">
             {socials.map((social) => (
               <li key={social.name}>
                 <a
