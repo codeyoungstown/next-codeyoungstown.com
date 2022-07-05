c3457c7e766be780df628ea0200712e9cf73b45e
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-07-05 18:51:58 -0400

    added html lang to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
