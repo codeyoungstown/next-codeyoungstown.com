05c3c18c1deb5b1dc13c3f4fb568faffc33b686f
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-07-10 09:08:54 -0400

    pushing changes before merging with link styling

--- pages/jobs-form.tsx
+++ pages/jobs-form.tsx
@@ -8,45 +8,58 @@ export default function JobsForm() {
       </h1>
 
       <form
-        className="text-blue"
+        className="p-4 flex-row"
         action="mailto:ryanlalchand@gmail.com"
         method="post"
         encType="text/plain"
       >
         <div>
-          <label htmlFor="name">Your Name: &nbsp;</label>
-          <input type="text" id="name" name="name" required />
+          <label htmlFor="name">Your Name: &nbsp;</label> <br />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-200
+                    focus:ring-0 focus:border-black"
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
         </div>
         <div>
-          <label htmlFor="email">Your Email: &nbsp;</label>
+          <label htmlFor="email">Your Email: &nbsp;</label> <br />
           <input type="email" id="email" name="email" required />
         </div>
         <div>
-          <label htmlFor="jobTitle">Job Title: &nbsp;</label>
+          <label htmlFor="jobTitle">Job Title: &nbsp;</label> <br />
           <input type="text" id="jobTitle" name="jobTitle" required />
         </div>
         <div>
-          <label htmlFor="company">Company: &nbsp;</label>
+          <label htmlFor="company">Company: &nbsp;</label> <br />
           <input type="text" id="company" name="company" required />
         </div>
         <div>
           <label htmlFor="location">
             Location (City, State OR Remote): &nbsp;
-          </label>
+          </label>{" "}
+          <br />
           <input type="text" id="location" name="location" required />
         </div>
         <div>
-          <label htmlFor="salaryRange">Salary Range (optional): &nbsp;</label>
+          <label htmlFor="salaryRange">Salary Range (optional): &nbsp;</label>{" "}
+          <br />
           <input type="text" id="salaryRange" name="salaryRange" />
         </div>
         <div>
           <label htmlFor="listingLink">
             Listing link or contact email (optional): &nbsp;
-          </label>
+          </label>{" "}
+          <br />
           <input type="text" id="listingLink" name="listingLink" />
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button className="p-4 " type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </Layout>
