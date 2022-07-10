e550e07f1b8dcf7c6570d14e8d43f85febf58adc
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2022-07-10 09:42:12 -0400

    made jobs form less ugly

--- pages/jobs-form.tsx
+++ pages/jobs-form.tsx
@@ -9,7 +9,8 @@ export default function JobsForm() {
 
       <form
         className="p-4 flex-row"
-        action="mailto:ryanlalchand@gmail.com"
+        action="mailto:ryanlalchand@gmail.com?subject=New Job Listing"
+        target="__blank"
         method="post"
         encType="text/plain"
       >
@@ -17,47 +18,110 @@ export default function JobsForm() {
           <label htmlFor="name">Your Name: &nbsp;</label> <br />
           <input
             className="bg-CYgray
-                    border-0 border-b-2 border-gray-200
-                    focus:ring-0 focus:border-black"
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
             type="text"
             id="name"
             name="name"
             required
-          />
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="email">Your Email: &nbsp;</label> <br />
-          <input type="email" id="email" name="email" required />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="email"
+            id="email"
+            name="email"
+            required
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="jobTitle">Job Title: &nbsp;</label> <br />
-          <input type="text" id="jobTitle" name="jobTitle" required />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="text"
+            id="jobTitle"
+            name="jobTitle"
+            required
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="company">Company: &nbsp;</label> <br />
-          <input type="text" id="company" name="company" required />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="text"
+            id="company"
+            name="company"
+            required
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="location">
             Location (City, State OR Remote): &nbsp;
           </label>{" "}
           <br />
-          <input type="text" id="location" name="location" required />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="text"
+            id="location"
+            name="location"
+            required
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="salaryRange">Salary Range (optional): &nbsp;</label>{" "}
           <br />
-          <input type="text" id="salaryRange" name="salaryRange" />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="text"
+            id="salaryRange"
+            name="salaryRange"
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
           <label htmlFor="listingLink">
             Listing link or contact email (optional): &nbsp;
           </label>{" "}
           <br />
-          <input type="text" id="listingLink" name="listingLink" />
+          <input
+            className="bg-CYgray
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+            type="text"
+            id="listingLink"
+            name="listingLink"
+          />{" "}
+          <br />
+          <br />
         </div>
         <div>
-          <button className="p-4 " type="submit">
+          <button
+            className="p-4 outline rounded-full border-0 border-white hover:bg-gray-700"
+            type="submit"
+          >
             Submit
           </button>
         </div>
