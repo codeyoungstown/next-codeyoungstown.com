1992774f5e19f3d9aee02ba7534180ac1201baae
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2022-07-19 10:50:35 -0400

    Add simple analytics (#31)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,25 @@ import "@fontsource/open-sans"; // Defaults to weight 400.
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false; // https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react#nextjs
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://sa.codeyoungstown.com/latest.js"
+        strategy="afterInteractive"
+      />
+      <noscript>
+        <img
+          src="https://sa.codeyoungstown.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </>
+  );
 }
 
 export default App;
