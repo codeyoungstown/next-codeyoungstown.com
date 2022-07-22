1e0bfbe5fdd1348d4788b59d74d6757ef7eb0dc4
Crow <garretthugs@gmail.com>
2022-07-21 20:45:57 -0400

    cleanup header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header>
-      <nav className="flex justify-center space-x-4 text-lg p-4">
+    <header className="flex justify-center">
+      <nav className="flex justify-center sm:w-3/4 sm:space-x-6 w-full space-x-4 p-3 bg-gray-600 rounded-xl items-center">
         {[
           ["Home", "/"],
           ["Made In Youngstown", "/made-in-youngstown"],
-          ["Jobs", "/jobs"],
+          // ["Jobs", "/jobs"],
         ].map(([title, url]) => (
           <Link href={url} key={title}>
-            <a className="hover:underline hover:text-gray-300">{title}</a>
+            <a className="hover:underline hover:text-gray-300 sm:text-2xl">
+              {title}
+            </a>
           </Link>
         ))}
       </nav>
