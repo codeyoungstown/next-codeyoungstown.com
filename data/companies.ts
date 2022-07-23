02ff25134f02422035269ae8feb1268fa3934c1f
Crow <garretthugs@gmail.com>
2022-07-23 19:27:00 -0400

    added descriptions to each company

--- data/companies.ts
+++ data/companies.ts
@@ -4,6 +4,7 @@ export const companies = [
     city: "Canfield",
     website: "https://45press.com/",
     careers: "",
+    desc: "We provide design, development and hosting expertise on-demand to help you build and host your next digital product, platform, or app.",
     servicesOrProduct: "Services",
   },
   {
@@ -11,6 +12,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.atpohio.com/",
     careers: "",
+    desc: "Advanced Technology Partners serves the IT needs of customers of all sizes all over the country with a core base of customers in Ohio and Pennsylvania.",
     servicesOrProduct: "Services",
   },
   {
@@ -18,6 +20,7 @@ export const companies = [
     city: "Girard",
     website: "https://www.aimntls.com/",
     careers: "http://www.jobsataim.com/",
+    desc: "At Aim, our highly skilled and dedicated team has the experience and technology to provide you with elite, award-winning commercial truck services that keep you and your business moving now and into the future.",
     servicesOrProduct: "Product",
   },
   {
@@ -25,6 +28,7 @@ export const companies = [
     city: "West Middlesex, PA",
     website: "https://autosoftdms.com/",
     careers: "https://autosoftdms.com/career-opportunities/",
+    desc: "Autosoft aims to support and empower dealerships of any size with proven solutions and practical knowledge to face the challenges and opportunities of the future.",
     servicesOrProduct: "Product",
   },
   {
@@ -32,6 +36,7 @@ export const companies = [
     city: "Warren",
     website: "https://www.avifoodsystems.com/",
     careers: "https://www.avifoodsystems.com/career-opportunities/",
+    desc: "We’re America’s largest, family-owned and operated food and hospitality provider, serving prestigious clients in the business, education, healthcare and leisure sectors.",
     servicesOrProduct: "Product",
   },
   {
@@ -39,6 +44,7 @@ export const companies = [
     city: "Sharon, PA",
     website: "https://www.blackknightinc.com/",
     careers: "https://www.blackknightinc.com/careers/",
+    desc: "Black Knight is a leading provider of integrated software, data and analytics solutions that facilitate and automate many of the business processes across the homeownership life cycle.",
     servicesOrProduct: "Services",
   },
   {
@@ -46,6 +52,7 @@ export const companies = [
     city: "Poland",
     website: "https://cboss.com/",
     careers: "",
+    desc: "We specialize in the design and implementation of industry leading gateway solutions for e-payment processing.",
     servicesOrProduct: "Product",
   },
   {
@@ -53,6 +60,7 @@ export const companies = [
     city: "Youngstown (& NE Ohio)",
     website: "https://cohenandcompany.com/",
     careers: "https://www.cohencpa.jobs/",
+    desc: "Top accounting and consulting firm specializing in audits and tax issues.",
     servicesOrProduct: "Services",
   },
   {
@@ -60,6 +68,7 @@ export const companies = [
     city: "Boardman",
     website: "https://creativestreammarketing.com/",
     careers: "",
+    desc: "We’re a marketing and creative agency, partnering with B2B and Retail Technology companies.",
     servicesOrProduct: "Services",
   },
   {
@@ -67,7 +76,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.drund.com/",
     careers: "https://www.drund.com/careers/",
-    desc: "We are an engineering company that provides technical solutions and strategies for our partners based on the Drund (c) platform.",
+    desc: "We are an engineering company that provides technical solutions and strategies for our partners based on the Drund platform.",
     tags: ["Web Dev", "App Dev", "UI/UX", "Marketing"],
     servicesOrProduct: "Product",
   },
@@ -77,6 +86,7 @@ export const companies = [
     website: "https://www.factset.com/",
     careers:
       "https://factset.wd1.myworkdayjobs.com/FactSetcareers/2/refreshFacet/318c8bb6f553100021d223d9780d30be?__hstc=&__hssc=&hsCtaTracking=5bad2784-dc05-40fe-a68d-fee1a12cdc9b%7C397e76f1-32f7-4fbf-bd20-d38c0c14c7a9",
+    desc: "FactSet creates data and software solutions for tens of thousands of investment professionals around the world.",
     servicesOrProduct: "Product",
   },
   {
@@ -84,6 +94,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.gbscorp.com/",
     careers: "https://gbscorp.isolvedhire.com/jobs/",
+    desc: "GBS is a leading information solutions provider, partnering with some of the world’s most established brand names.",
     servicesOrProduct: "Product",
   },
   {
@@ -91,6 +102,7 @@ export const companies = [
     city: "Fredonia, PA",
     website: "https://www.graceindustries.com/",
     careers: "",
+    desc: "Grace Industries, Inc. is a manufacturing company who produces safety and life saving equipment.",
     servicesOrProduct: "Product",
   },
   {
@@ -98,6 +110,7 @@ export const companies = [
     city: "Poland",
     website: "https://idmi.net/",
     careers: "https://idmi.net/careers",
+    desc: "IDMI.Net provides comprehensive, customized digital footprints for businesses and organizations large and small.",
     servicesOrProduct: "Services",
   },
   {
@@ -105,6 +118,7 @@ export const companies = [
     city: "Poland",
     website: "https://intellitechcorporation.com/",
     careers: "",
+    desc: "Intellitech is a provider of innovative Jail and public safety software solutions.",
     servicesOrProduct: "Product",
   },
   {
@@ -112,6 +126,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.involta.com/",
     careers: "https://www.involta.com/careers",
+    desc: "Involta offers cloud solutions, infrastructure, consulting and data centers.",
     servicesOrProduct: "Services",
   },
   {
@@ -119,6 +134,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://jetcreative.com/",
     careers: "",
+    desc: "Jet is a creative design and marketing agency dedicated to the growth of businesses and organizations in Northeast Ohio.",
     servicesOrProduct: "Services",
   },
   {
@@ -127,6 +143,7 @@ export const companies = [
     website: "https://lordstownmotors.com/",
     careers:
       "https://recruiting.paylocity.com/Recruiting/Jobs/All/ddd48b68-5d30-4845-bd31-2cbbac035ac6/Lordstown-Motors-Corp",
+    desc: "Lordstown Motors builds electric pickup trucks and other vehicles to revolutionize the way work gets done.",
     servicesOrProduct: "Product",
   },
   {
@@ -134,13 +151,15 @@ export const companies = [
     city: "Boardman",
     website: "https://www.neweratech.com/us/",
     careers: "",
+    desc: "New Era Technology works with customers as a trusted technology adviser.",
     servicesOrProduct: "Product",
   },
   {
     name: "Palo Creative",
     city: "Youngstown",
     website: "https://www.palocreative.com/",
-    careers: "",
+    careers: "https://www.palocreative.com/careers",
+    desc: "PALO Creative is a advertising and digital media agency specializing in traditional and digital marketing solutions.",
     servicesOrProduct: "Services",
   },
   {
@@ -148,6 +167,7 @@ export const companies = [
     city: "Poland",
     website: "https://www.pdmi.com/",
     careers: "",
+    desc: "Provides technology and other specialized industry solutions for managing health.",
     servicesOrProduct: "Product",
   },
   {
@@ -155,6 +175,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://fireworks.com/",
     careers: "",
+    desc: "Phantom Fireworks is America's premier consumer firework retailer.",
     servicesOrProduct: "Product",
   },
   {
@@ -162,6 +183,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://sensourceinc.com/",
     careers: "",
+    desc: "SenSource provides people counting solutions and traffic analytics software solutions.",
     servicesOrProduct: "Product",
   },
   {
@@ -169,6 +191,7 @@ export const companies = [
     city: "Poland",
     website: "http://serverightsoftware.com/",
     careers: "",
+    desc: "ServeRight provides advanced online ordering solutions.",
     servicesOrProduct: "Product",
   },
   {
@@ -176,6 +199,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.simonroofing.com/",
     careers: "https://www.simonroofing.com/careers/",
+    desc: "Simon Roofing manufactures and installs the commercial roofing products used for repairs, restorations and replacements.",
     servicesOrProduct: "Product",
   },
   {
@@ -183,6 +207,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.turning.com/",
     careers: "https://www.turning.com/careers",
+    desc: "Turning Technologies is an education technology company.",
     servicesOrProduct: "Product",
   },
   {
@@ -191,6 +216,7 @@ export const companies = [
     website: "https://www.ultiumcell.com/",
     careers:
       "https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=cdd81bde-c04d-4821-8470-4bfe169d4493&ccId=9200095921795_2&type=JS&lang=en_US",
+    desc: "Ultium Cells are a modern producer of battery cells designed for electric vehicles.",
     servicesOrProduct: "Product",
   },
   {
@@ -198,6 +224,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://www.vingapp.com/",
     careers: "",
+    desc: "Ving is a compliance and training monitoring platform.",
     servicesOrProduct: "Product",
   },
   {
@@ -205,6 +232,7 @@ export const companies = [
     city: "Youngstown",
     website: "https://ysu.edu/",
     careers: "https://ysu.edu/human-resources/",
+    desc: "Youngstown State University is a public university in Youngstown, Ohio.",
     servicesOrProduct: "Product",
   },
   {
@@ -212,6 +240,7 @@ export const companies = [
     city: "Hermitage, PA",
     website: "https://www.fnb-online.com/",
     careers: "https://fnbcorp.wd1.myworkdayjobs.com/FNBCORP",
+    desc: "FNB provides a full range of commercial banking, consumer banking and wealth management services.",
     servicesOrProduct: "Services",
   },
   {
@@ -219,6 +248,7 @@ export const companies = [
     city: "Hermitage, PA",
     website: "https://www2.deloitte.com/us/en.html",
     careers: "https://www2.deloitte.com/us/en/careers/careers.html",
+    desc: "Deloitte provides industry-leading audit, consulting, tax, and advisory services.",
     servicesOrProduct: "Services",
   },
   {
@@ -226,6 +256,7 @@ export const companies = [
     city: "West Middlesex, PA",
     website: "https://www.wernerworldwide.com/",
     careers: "https://www.wernerworldwide.com/en/careers/job-postings/usa",
+    desc: "WernerCo is an international manufacturer and distributor of climbing products, access systems, fall protection equipment and jobsite truck and van storage equipment.",
     servicesOrProduct: "Product",
   },
   {
@@ -233,6 +264,7 @@ export const companies = [
     city: "Poland, OH",
     website: "https://deltatelecomohio.com",
     careers: "330.757.8276 fax",
+    desc: "Delta Telecom is your local choice for business telephone services, internet, fiber optics, and more.",
     servicesOrProduct: "Services",
   },
 ];
