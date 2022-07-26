43cbb937c08100dbbb4de508f8313ffb8331ab26
Crow <garretthugs@gmail.com>
2022-07-25 21:58:38 -0400

    fixes

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -1,33 +1,11 @@
 import { companies } from "../data/companies";
-import DataCard from "./DataCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import CareerLink from "./CareerLink";
+import WebsiteLink from "./WebsiteLink";
+import Company from "./Company";
 
 export default function Companies() {
-  const CareerPage = ({ link }) => {
-    return (
-      <a
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-        className="text-colt hover:text-[#826e62] font-bold pl-4"
-      >
-        CAREERS
-      </a>
-    );
-  };
-  const Website = ({ link }) => {
-    return (
-      <a
-        href={link}
-        target="_blank"
-        rel="noreferrer"
-        className="text-colt hover:text-[#826e62] font-bold"
-      >
-        WEBSITE
-      </a>
-    );
-  };
   const Tag = ({ children }) => {
     return (
       <div className="bg-stone-900 rounded-md px-2 py-[2px] mr-2 min-w-max">
@@ -40,29 +18,27 @@ export default function Companies() {
   return (
     <div className="flex flex-col w-full sm:grid sm:grid-cols-2 sm:gap-4 sm:content-start lg:grid-cols-3">
       {companies.map((company) => (
-        <DataCard key={company.name}>
-          <p className="text-xl font-medium">{company.name}</p>
+        <Company key={company.name}>
+          <h2 className="text-xl font-medium">{company.name}</h2>
           <p className="text-gray-400">
             <FontAwesomeIcon icon={faLocationDot} /> {company.city}
           </p>
           <p className="leading-5 my-2">{company.desc}</p>
           <div className="flex overflow-auto my-1">
-            {company.tags ? (
-              company.tags.map((item) => <Tag key={item}>{item}</Tag>)
-            ) : (
-              <></>
-            )}
+            {company.tags
+              ? company.tags.map((item) => <Tag key={item}>{item}</Tag>)
+              : null}
           </div>
           <div className="grid grid-cols-2 mt-auto">
             <div className="col-span-1">
-              <Website link={company.website} />
-              {company.careers ? <CareerPage link={company.careers} /> : <></>}
+              <WebsiteLink link={company.website} />
+              {company.careers ? <CareerLink link={company.careers} /> : null}
             </div>
             <p className="italic text-gray-400 text-right col-span-2 sm:col-span-1">
               {company.servicesOrProduct}
             </p>
           </div>
-        </DataCard>
+        </Company>
       ))}
     </div>
   );
--- components/Company.tsx
+++ components/Company.tsx
@@ -3,7 +3,7 @@
     Local Jobs: title, company, location, salary?, listing link/contact email
 */
 
-export default function DataCard({ children }) {
+export default function Company({ children }) {
   return (
     <div className="shadow-black shadow-md rounded overflow-hidden border border-white m-4">
       <div className="flex flex-col h-full p-4 leading-8 text-left">
