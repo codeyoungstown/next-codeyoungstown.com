c78be578f59c24ee712373ea76fde01a58effff9
Crow <garretthugs@gmail.com>
2022-09-06 19:01:52 -0400

    improved css of navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import Image from "next/image";
 
 const Pages = [
   ["HOME", "/"],
@@ -17,7 +18,7 @@ export default function Header() {
   };
 
   return (
-    <header className="flex justify-center mb-3">
+    <header className="mb-3">
       <nav className="sm:flex justify-center space-x-4 p-3 items-center hidden">
         {Pages.map(([title, url]) => (
           <Link href={url} key={title}>
@@ -29,28 +30,24 @@ export default function Header() {
       </nav>
 
       {/* Mobile Menu */}
-      <nav className="sm:hidden w-full p-2">
-        <div className="grid grid-cols-5">
-          <div className="flex justify-start col-span-4">
-            <Link href="/">
-              <a className="text-2xl text-gray-400">Code Youngstown</a>
-            </Link>
-          </div>
-          <div className="flex justify-end col-span-1">
-            <button
-              className="text-gray-400 text-xl py-1 px-4 rounded-md"
-              onClick={ToggleMenu}
-            >
-              <FontAwesomeIcon icon={faBars} />
-            </button>
-          </div>
-        </div>
+      <nav className="sm:hidden flex flex-wrap justify-between items-center pt-1">
+        <Link href="/">
+          <a className="ml-2 text-2xl text-left text-gray-400">
+            Code Youngstown
+          </a>
+        </Link>
+        <button
+          className="text-gray-400 text-xl mr-4 flex text-right"
+          onClick={ToggleMenu}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
 
-        <div id="mobile-nav-links" className={`w-full mt-2 ${menuLinks}`}>
+        <div className={`w-full mt-2 ${menuLinks}`}>
           <ul className="w-full">
             {Pages.map(([title, url]) => (
               <Link key={title} href={url}>
-                <li className="hover:underline hover:text-gray-300 border p-2">
+                <li className="hover:underline hover:text-gray-300 border p-1">
                   <a className="text-gray-400 text-xl">{title}</a>
                 </li>
               </Link>
