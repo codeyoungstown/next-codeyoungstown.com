debe8ff5d886ba43447fa3ae41f283a8a23ecf8b
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2022-09-22 13:04:46 -0400

    add type and not required props

--- components/Globals/FormField.tsx
+++ components/Globals/FormField.tsx
@@ -3,13 +3,27 @@ interface FormProps {
   label: string;
   textarea?: boolean;
   type?: string;
+  notRequired?: boolean;
 }
 
-export default function FormField({ name, label, textarea }: FormProps) {
+export default function FormField({
+  name,
+  label,
+  textarea,
+  type,
+  notRequired,
+}: FormProps) {
+  notRequired = notRequired ? true : false;
+
   return textarea ? (
     <TextArea name={name} label={label} />
   ) : (
-    <TextField name={name} label={label} />
+    <TextField
+      name={name}
+      label={label}
+      type={type}
+      notRequired={notRequired}
+    />
   );
 }
 
@@ -31,21 +45,40 @@ const TextArea = ({ name, label }) => {
   );
 };
 
-const TextField = ({ name, label }) => {
-  return (
-    <div className="w-full my-2 leading-4">
-      <label className="w-full block text-left my-2" htmlFor={name}>
-        {label}
-      </label>
-      <input
-        className="bg-gray-600 p-1 w-full mb-2 rounded-md
+const TextField = ({ name, label, type, notRequired }) => {
+  type = type ? type : "text";
+  if (notRequired) {
+    return (
+      <div className="w-full my-2 leading-4">
+        <label className="w-full block text-left my-2" htmlFor={name}>
+          {label}
+        </label>
+        <input
+          className="bg-gray-600 p-1 w-full mb-2 rounded-md
                     border-0 border-b-2 border-gray-500
                     focus:ring-0 focus:border-gray-100"
-        id={name}
-        name={name}
-        type="text"
-        required
-      />
-    </div>
-  );
+          id={name}
+          name={name}
+          type={type}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full my-2 leading-4">
+        <label className="w-full block text-left my-2" htmlFor={name}>
+          {label}
+        </label>
+        <input
+          className="bg-gray-600 p-1 w-full mb-2 rounded-md
+                    border-0 border-b-2 border-gray-500
+                    focus:ring-0 focus:border-gray-100"
+          id={name}
+          name={name}
+          type={type}
+          required
+        />
+      </div>
+    );
+  }
 };
