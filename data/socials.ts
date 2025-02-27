0501e6e721abf6b82ebb445b7f1b5aeb934a8198
Nicholas Serra <nickserra@gmail.com>
2025-02-27 00:10:21 -0500

    Update second slack link

--- data/socials.ts
+++ data/socials.ts
@@ -19,7 +19,7 @@ export type Social = {
 export const socials: Social[] = [
   {
     name: "Slack",
-    link: "http://slack.codeyoungstown.com/",
+    link: "https://join.slack.com/t/codeyoungstown/shared_invite/zt-1rf69qgvp-2YQTU4dPDMnsGwFEOJ3Xug",
     icon: faSlack,
   },
   {
